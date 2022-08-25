fd1495b4d938c9d1fca0b2677d7e39b827e04b5b
menxqk <menxqk@gmail.com>
2022-08-25 19:59:47 -0300

    lists and dicts

---
+++ ex33.py
@@ -0,0 +1,17 @@
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top is is {i}")
+    numbers.append(i)
+
+    i = i + 1
+
+    print("Numbers now: ", numbers)
+    print(f"At the bottom is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
